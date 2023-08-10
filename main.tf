dfc08676e285653ae479d112a9734cb6b5b5b174
Yiannis <8741709+yiannistri@users.noreply.github.com>
2023-08-10 13:43:40 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.3.9"
+}
+
+variable "subject" {
+   type = string
+   default = "World"
+   description = "Subject to hello"
+}
+
+output "hello_world" {
+  value = "Hello branch planner demo, ${var.subject}!"
+}
